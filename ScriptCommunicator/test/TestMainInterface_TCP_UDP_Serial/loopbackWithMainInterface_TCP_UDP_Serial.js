e742709dd39d729e4eb3eef2a95a3e72bf3f65ed
szieke <s.zieker@gmx.net>
2017-08-04 19:34:27 +0200

    no message

--- ScriptCommunicator/test/TestMainInterface_TCP_UDP_Serial/loopbackWithMainInterface_TCP_UDP_Serial.js
+++ ScriptCommunicator/test/TestMainInterface_TCP_UDP_Serial/loopbackWithMainInterface_TCP_UDP_Serial.js
@@ -7,9 +7,17 @@ function stopScript()
 
 function dataReceived(data)
 {
-	scriptInf.sendDataArray(data);
+	g_receivedData = g_receivedData.concat(data);
+	
+	if((g_receivedData.length > 200) || (g_receivedData.indexOf(10) != -1))
+	{
+		scriptInf.sendDataArray(g_receivedData);
+		g_receivedData = Array();
+	}
+
 }
 
+var g_receivedData = Array();
 scriptThread.appendTextToConsole('loopback script has started');
 scriptInf.dataReceivedSignal.connect(dataReceived);
 
